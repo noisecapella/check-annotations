f0edd816875e921e9735e5c1284d27b9892a3883
George Schneeloch <bostonbusmap@gmail.com>
2013-04-27 15:53:41 -0400

    Add print module

--- check-annotations.py
+++ check-annotations.py
@@ -2,6 +2,12 @@ import argparse
 import os
 import os.path
 
+from logilab.astng import builder
+from logilab.astng.utils import ASTWalker
+from logilab.astng.scoped_nodes import Module
+
+abuilder = builder.ASTNGBuilder()
+
 def make_paths(path) -> list:
     """returns a collection of python file paths"""
 
@@ -15,14 +21,32 @@ def make_paths(path) -> list:
     else:
         raise Exception("%s is not a directory or a file" % path)
 
+class PrintAll:
+    def set_context(self, node, child_node):
+        pass
+
+    def visit_module(self, node):
+        print(node.as_string())
+
+def print_module(path):
+    print("In path %s" % path)
+    with open(path) as f:
+        code = f.read()
+    obj = abuilder.string_build(code)
+    ASTWalker(PrintAll()).walk(obj)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Statically check types for consistency')
     parser.add_argument('path', type=str, help='path to directory containing Python files,'
                                                ' or a path to a single Python file')
+    parser.add_argument("--print", action='store_true')
     args = parser.parse_args()
 
     paths = make_paths(args.path)
-    print(list(paths))
+    if args.print:
+        for path in paths:
+            print_module(path)
 
 if __name__ == "__main__":
     main()
