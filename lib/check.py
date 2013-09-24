d8b58727f754a18a8b313ab8dbe84631dc4e953d
George Schneeloch <bostonbusmap@gmail.com>
2013-09-23 20:24:12 -0400

    If inference detects two different non-convertable types, throw an exception

--- lib/check.py
+++ lib/check.py
@@ -14,6 +14,37 @@ def infer(node):
     except InferenceError:
         return ""
 
+def convertible(from_type, to_type):
+    if from_type == to_type:
+        return True
+    if ((from_type == int and to_type == float) or
+        (from_type == float and to_type == int)):
+        return True
+    if from_type == str or to_type == str:
+        return False
+    try:
+        if issubclass(from_type, to_type) or issubclass(to_type, from_type):
+            return True
+    except TypeError:
+        pass
+    # TODO: this is pretty arbitrary where this line is drawn
+    return False
+
+def collision_in(infer_list):
+    already_found = []
+    for item in infer_list:
+        item_type = item.pytype()
+        if not already_found:
+            already_found.append(item_type)
+        else:
+            for other_type in already_found:
+                if not convertible(item_type, other_type):
+                    raise Exception("Found type %s which conflicts with type %s" % (item_type, other_type))
+            if item_type not in already_found:
+                already_found.append(item_type)
+
+    return False
+
 class MatchFunc:
     def __init__(self, path):
         self.path = path
@@ -28,13 +59,8 @@ class MatchFunc:
 
         """
         
-        #context = CallContext(node.args, node.starargs, node.kwargs)
-        print("%s %s" % (node.name, list(node.infer())))
-
-    def visit_callfunc(self, node):
-        node_func = node.func
-        infer = list(node_func.infer())
-        print("Callfunc: %s %s %s" % (node.func, node, infer))
+        if collision_in(node.infer()):
+            raise Exception("Collision: %s" % node.infer)
 
 
 
